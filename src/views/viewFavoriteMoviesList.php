0f766a7b544fae51fde51255b8303e39da8439fe
Bongchiam85 <athlan_brice@yahoo.fr>
2016-12-09 15:34:20 +0100

    vues

---
+++ src/views/viewFavoriteMoviesList.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Espace personnel - Films préférés</title>
+        <link type="text/css" href="css/cinema.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <header><h1><?= $utilisateur['prenom'] ?> <?= $utilisateur['nom'] ?>, ci-dessous vos films préférés</h1></header>
+        <table class="std">
+            <tr>
+                <th>Titre</th>
+                <th>Commentaire</th>
+                <th colspan="2">Action</th>
+            </tr>
+            <?php
+            // on récupère la liste des films préférés grâce à l'utilisateur identifié
+            $films = $fctManager->getFavoriteMoviesFromUser($utilisateur['userID']);
+            // si des films ont été trouvés
+            if ($films) {
+                // boucle de création du tableau
+                foreach ($films as $film) {
+                    ?>
+                    <tr>
+                        <td><?= $film['titre'] ?></td>
+                        <td><?= $film['commentaire'] ?></td>
+                        <td>
+                            <form name="modifyFavoriteMovie" action="editFavoriteMovie.php" method="GET">
+                                <input type="hidden" name="userID" value="<?= $utilisateur['userID'] ?>"/>
+                                <input type="hidden" name="filmID" value="<?= $film['filmID'] ?>"/>
+                                <input type="image" src="images/modifyIcon.png" alt="Modify"/>
+                            </form>
+                        </td>
+                        <td>
+                            <form name="deleteFavoriteMovie" action="deleteFavoriteMovie.php" method="POST">
+                                <input type="hidden" name="userID" value="<?= $utilisateur['userID'] ?>"/>
+                                <input type="hidden" name="filmID" value="<?= $film['filmID'] ?>"/>
+                                <input type="image" src="images/deleteIcon.png" alt="Delete"/>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            }
+            ?>
+            <tr class="new">
+                <td colspan="4">
+                    <form name="addFavoriteMovie" action="editFavoriteMovie.php">
+                        <button class="add" type="submit">Cliquer pour ajouter un film préféré...</button>
+                    </form>
+                </td>
+            </tr>        
+        </table>
+
+        <form name="backToMainPage" action="index.php">
+            <input type="submit" value="Retour à l'accueil"/>
+        </form>
+    </body>
+</html>
---
+++ src/views/viewMoviesList.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Gestion des cinémas - Films</title>
+        <link type="text/css" href="css/cinema.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <header><h1>Liste des films</h1></header>
+        <table class="std">
+            <tr>
+                <th>Titre</th>
+                <th>Titre original</th>
+                <th colspan="3">Action</th>
+            </tr>
+            <?php
+            // on récupère la liste des films ainsi que leurs informations
+            $films = $fctManager->getMoviesList();
+            // boucle de construction de la liste des cinémas
+            foreach ($films as $film) {
+                ?>
+                <tr>
+                    <td><?= $film['TITRE'] ?></td>
+                    <td><?= $film['TITREORIGINAL'] ?></td>
+                    <td>
+                        <form name="movieShowtimes" action="movieShowtimes.php" method="GET">
+                            <input name="filmID" type="hidden" value="<?= $film['FILMID'] ?>"/>
+                            <input type="submit" value="Consulter les séances"/>
+                        </form>
+                    </td>
+                    <?php if ($isUserAdmin): ?>
+                        <td>
+                            <form name="modifyMovie" action="editMovie.php" method="GET">
+                                <input type="hidden" name="filmID" value="<?= $film['FILMID'] ?>"/>
+                                <input type="image" src="images/modifyIcon.png" alt="Modify"/>
+                            </form>
+                        </td>
+                        <td>
+                            <form name="deleteMovie" action="deleteMovie.php" method="POST">
+                                <input type="hidden" name="filmID" value="<?= $film['FILMID'] ?>"/>
+                                <input type="image" src="images/deleteIcon.png" alt="Delete"/>
+                            </form>
+                        </td>
+                    <?php endif; ?>
+                </tr>
+                <?php
+            }
+            ?>
+            <?php if ($isUserAdmin): ?>
+                <tr class="new">
+                    <td colspan="5">
+                        <form name="addMovie" action="editMovie.php">
+                            <button class="add" type="submit">Cliquer ici pour ajouter un film...</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endif; ?>
+        </table>
+        <form name="backToMainPage" action="index.php">
+            <input type="submit" value="Retour à l'accueil"/>
+        </form>
+    </body>
+</html>
