5a7080a4cd34f970f82a6248258ea7a6603f3434
teambote <lou.yetu@laposte.net>
2016-12-12 16:09:26 +0100

    mvc-iteration-02-005

--- src/index.php
+++ src/index.php
@@ -9,21 +9,86 @@ require_once __DIR__ . './includes/Manager.php';
 require_once __DIR__ . './init.php';
 
 //require_once __DIR__ . './includes/managers.php';
-
 // appel au contrôleur serviteur
 require __DIR__ . './controllers/controleur.php';
 
 // on "sainifie" les entrées
 $sanitizedEntries = filter_input_array(INPUT_GET, ['action' => FILTER_SANITIZE_STRING]);
 if ($sanitizedEntries && $sanitizedEntries['action'] !== '') {
-    // si l'action demandée est la liste des cinémas
-    if ($sanitizedEntries['action'] == "cinemasList") {
-        // Activation de la route cinemasList
-        cinemasList($managers);
-    } else {
-        // Activation de la route par défaut (page d'accueil)
-        home($managers);
+    switch ($sanitizedEntries['action']) {
+
+        case "cinemasList":
+            // Activation de la route cinemasList
+            cinemasList($managers);
+            break;
+        case "moviesList":
+            // Activation de la route moviesList
+            moviesList($managers);
+            break;
+        case "movieShowTimes":
+            // Activation de la route movieShowTimes
+            movieShowTimes($managers);
+            break;
+        case "editShowTime":
+            // Activation de la route editShowTime
+            editShowTime($managers);
+            break;
+        case "editMovie":
+            // Activation de la route editMovie
+            editMovie($managers);
+            break;
+        case "editFavoriteMoviesList":
+            // Activation de la route editFavoriteMoviesList
+            editFavoriteMoviesList($managers);
+            break;
+        case "editFavoriteMovie":
+            // Activation de la route editFavoriteMovie
+            editFavoriteMovie($managers);
+            break;
+        case "editCinema":
+            // Activation de la route editCinema
+            editCinema($managers);
+            break;
+        case "deleteShowtime":
+            // Activation de la route deleteShowtime
+            deleteShowtime($managers);
+            break;
+        case "deleteMovie":
+            // Activation de la route deleteMovie
+            deleteMovie($managers);
+            break;
+        case "deleteFavoriteMovie":
+            // Activation de la route deleteFavoriteMovie
+            deleteFavoriteMovie($managers);
+            break;
+        case "deleteCinema":
+            // Activation de la route deleteCinema
+            deleteCinema($managers);
+            break;
+        case "createNewUser":
+            // Activation de la route CreateNewUser
+            CreateNewUser($managers);
+            break;
+        case "cinemaShowTimes":
+            // Activation de la route cinemaShowTimes
+            cinemaShowTimes($managers);
+            break;
+        default:
+            // Activation de la route par défaut (page d'accueil)
+            home($managers);
     }
+
+    /*
+      // si l'action demandée est la liste des cinémas
+      if ($sanitizedEntries['action'] == "cinemasList") {
+      // Activation de la route cinemasList
+      cinemasList($managers);
+      } else {
+      // Activation de la route par défaut (page d'accueil)
+      home($managers);
+      }
+     * 
+     */
 } else {
     // Activation de la route par défaut (page d'accueil)
     home($managers);
